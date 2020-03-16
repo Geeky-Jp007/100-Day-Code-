650e46b5f573fa8aa699e258ced1a4cd8359a476
Jaspreet Singh <singhjaspreet490@gmail.com>
2020-03-17 00:02:41 +0530

    edition

--- BST/BASIC.cpp
+++ BST/BASIC.cpp
@@ -15,6 +15,48 @@ class bst{
         else root->right = insert(root->right,value);
         return root;
     }
+    bst* deleteval(bst* root,int value){
+        if(root==NULL) return root;
+
+        if(root->data > value){
+            root->left = deleteval(root->left, value);
+            return root;
+        }
+        else if (root->data < value){
+            root->right = deleteval(root->right, value);
+            return root;
+        }
+        //found root!!!
+        if(root->left == NULL){
+            bst* temp = root->right;
+            delete root;
+            return temp;
+        }
+        else if(root->right == NULL){
+            bst* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        //if root has two child we check for successor and right elements 
+        //of successor is connected to left of parent of successor.
+
+        else{
+            bst *successor = root->right;
+            bst *parent_successor = root->right;
+
+            while(successor->left!=NULL){
+                parent_successor = successor;
+                successor = successor->left;
+            }
+
+            parent_successor->left = successor->right;
+            root->data = successor->data;
+            delete successor;
+            return root;
+        }
+
+    }
     void inorder(bst* root){
         if(root==NULL) return;
         inorder(root->left);
@@ -31,7 +73,16 @@ int main(){
         if(!i) root = b.insert(root,data);
         else b.insert(root,data);
     }
+    
     b.inorder(root);
+    cout<<endl;
+    int del;
+    cin>>del;
+
+    b.deleteval(root,del);
+
+    b.inorder(root);
+
     cout<<endl;
     return 0;
 }
